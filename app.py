55def0a21ed796a6f0e881ebd5783a1807a461cf
Sander C. Braathen <sanderbraathen67@gmail.com>
2025-05-14 11:04:03 +0200

    changed what port the app runs at

--- app.py
+++ app.py
@@ -167,4 +167,4 @@ def cart():
     return render_template('cart.html', cart_items=cart_items, total=total, first_name=first_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
